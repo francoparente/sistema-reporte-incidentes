400f63ce132be657dda00b60333246d10a5c8280
Franco Parente <fparente14@frba.utn.edu.ar>
2021-11-19 17:20:20 -0300

    first commit

---
+++ app/Http/Controllers/ClientesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientesController extends Controller
+{
+    //
+}
+
+/*
+Para crear un controlador uso el siguiente comando en consola:
+    php artisan make: controller [NombreControlador]
+*/
---
+++ resources/views/intro.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Intro</title>
+</head>
+<body style="background-color: #1f1f1f" class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="d-flex justify-content-center">
+        <h1 style="color: gray;">Mi primer proyecto con</h1>
+        <img src="https://laravel.com/img/logotype.min.svg" alt="Laravel" class="mx-3">
+    </div>
+    <br>   
+    <div class="text-center">
+        <button type="button" class="btn btn-danger" role="link" onclick="window.location='/fullstack-utn-laravel-project/public/welcome'">
+            Ir a Laravel Homepage
+        </button>
+    </div>
+   
+
+
+</body>
+</html>
